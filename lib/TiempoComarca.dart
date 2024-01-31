e93c1a0f4bb7c5b868b301b72119a841964c357b
Nebot774 <adriclemenbot@gmail.com>
2024-01-31 10:34:39 +0100

    Commit:Pantalles informacio comarques finiquitado

--- lib/TiempoComarca.dart
+++ lib/TiempoComarca.dart
@@ -9,34 +9,44 @@ class TiempoComarca extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Obtiene las dimensiones de la pantalla
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Tiempo en la Comarca"),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Image.asset('assets/tiempo.png'), // Asegúrate de que esta imagen exista en tus assets
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              "Población: ${comarcaData["poblacio"]}",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 16 / 9, // Define la relación de aspecto de la imagen
+              child: Image.asset(
+                'assets/tiempo2.png',
+                fit: BoxFit.cover, // Cubre el espacio mientras mantiene las proporciones
+                width: MediaQuery.of(context).size.width, // Ancho de la pantalla
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text("Latitud: ${comarcaData["coordenades"][0]}"),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text("Longitud: ${comarcaData["coordenades"][1]}"),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Población: ${comarcaData["poblacio"]}"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Latitud: ${comarcaData["coordenades"][0]}"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Longitud: ${comarcaData["coordenades"][1]}"),
+            ),
+            // Aquí puedes añadir más widgets si es necesario
+          ],
+        ),
       ),
     );
   }
 }
+
